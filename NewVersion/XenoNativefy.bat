9be9a420876778101442f8966f6ec75e6deda0a9
baldash <hugo.baldassin@epitech.eu>
2019-11-18 15:39:28 +0100

    [EDIT] added publisher in script instead of configuration

--- NewVersion/XenoNativefy.bat
+++ NewVersion/XenoNativefy.bat
@@ -20,3 +20,4 @@ pause
 
 :error
 exit /b %errorlevel%
+pause
--- NewVersion/XenoWSPackage.bat
+++ NewVersion/XenoWSPackage.bat
@@ -4,6 +4,7 @@
 set publisherDisplayName="Ask Technologies, Inc."
 set identityName="AskTechnologiesInc.289824193446D"
 set appName=XenoApp
+set publisher=CN=8D9F185A-3885-4A4D-82B6-4215CABDDBA7
 
 :: comment this if you don't want the created installation files (output folder) to be removed
 set postInstallClean=y
@@ -12,7 +13,7 @@ set postInstallClean=y
 cmd /C if exist .\output\ rd /q /s .\output\ || goto :error 
 
 :: main command -> packages the nativefier app for the windows store
-cmd /C electron-windows-store --publisher-display-name=%publisherDisplayName% --identity-name %identityName% --assets XenoApp-win32-x64\assets --input-directory %appName%-win32-x64/ --output-directory %appName% --package-version 1.0.0.0 --package-name %appName% --package-display-name %appName% || goto :error 
+cmd /C electron-windows-store --publisher %publisher% --publisher-display-name=%publisherDisplayName% --identity-name %identityName% --assets XenoApp-win32-x64\assets --input-directory %appName%-win32-x64/ --output-directory %appName% --package-version 1.0.0.0 --package-name %appName% --package-display-name %appName% || goto :error 
 
 :: because of some weird icon issues we need to unpack the app, create some
 :: uwp configs for the assets (makepri commands), and then repack the app
@@ -48,3 +49,4 @@ pause
 
 :error
 exit /b %errorlevel%
+pause
